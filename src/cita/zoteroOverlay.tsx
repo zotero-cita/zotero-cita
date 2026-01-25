zotero-cita/zotero-cita

commit 3f5ae6697c644a16e8a973c82a87d5d6ead6d852

Enable adding item by identifier with QID (#335)
* Enable adding a Zotero item with a QID
Fixes #236

* Add comments to code

src/cita/zoteroOverlay.tsx
@@ -20,7 +20,8 @@ import { MenuitemOptions } from "zotero-plugin-toolkit";
 import { Citation } from "./citation";
 import { IndexerBase } from "./indexers/indexer";
 import PIDBoxContainer from "../containers/pidBoxContainer";
-import OCI from "../oci";
+import { patch, unpatch } from "../utils/patcher";
+import wikicite from "./wikicite";
 
 const TRANSLATORS_PATH = `chrome://${config.addonRef}/content/translators`;
 const TRANSLATOR_LABELS = [
@@ -74,6 +75,8 @@ class ZoteroOverlay {
 		this.addPreferenceUpdateObservers();
 
 		this.installTranslators();
+
+		this.patchEnableAddingItemByIdenfitiferWithQID();
 	}
 
 	unload() {
@@ -84,6 +87,8 @@ class ZoteroOverlay {
 		this.removePreferenceUpdateObservers();
 
 		this.uninstallTranslators();
+
+		this.unpatchEnableAddingItemByIdentifierWithQID();
 	}
 
 	/******************************************/
@@ -235,6 +240,54 @@ class ZoteroOverlay {
 		} catch (err) {
 			ztoolkit.log(`Failed to remove translator ${label}`, err as Error);
 		}
+	}
+
+	private patchEnableAddingItemByIdenfitiferWithQID() {
+		// see #236
+		// try to extract QIDs from the textbox
+		// if none are found, try to extract other identifiers as normal
+		patch(
+			Zotero.Utilities,
+			"extractIdentifiers",
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+			(original: Function) =>
+				function Zotero_Utilities_extractIdentifiers(text: string) {
+					const identifiers = Wikidata.extractQIDsFromText(text);
+
+					if (!identifiers.length) {
+						return original(text);
+					} else {
+						return identifiers;
+					}
+				},
+		);
+		// we need to pass the QIDs to the translator
+		// by default they aren't recognised and an error is raised
+		patch(
+			Zotero.Translate.Search.prototype,
+			"setIdentifier",
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+			(original: Function) =>
+				function Zotero_Translate_Search_prototype_setIdentifier(
+					this: any,
+					identifier: {
+						[type: string]: string;
+					},
+				) {
+					// QIDs are stored as {extra: "qid: Q...."}
+					// see Wikidata.getItems
+					if (identifier.extra) {
+						this.setSearch([identifier]);
+					} else {
+						original(text);
+					}
+				},
+		);
+	}
+
+	private unpatchEnableAddingItemByIdentifierWithQID() {
+		unpatch(Zotero.Utilities, "extractIdentifiers");
+		unpatch(Zotero.Translate.Search.prototype, "setIdentifier");
 	}
 
 	/******************************************/
@@ -1049,7 +1102,7 @@ class ZoteroOverlay {
 							sourceItem?.citations[this._citationIndex!];
 						const targetItem = citation?.target;
 						return !!targetItem?.qid;
-					}
+					},
 				},
 				// Export to file
 				{

src/cita/wikidata.ts
@@ -416,6 +416,25 @@ export default class {
 		return qids;
 	}
 
+	// based on Zotero.Utilities.extractIdentifiers
+	static extractQIDsFromText(text: string) {
+		const foundIDs = new Set(); // keep track of identifiers to avoid duplicates
+		const identifiers: { [idenifier: string]: string }[] = [];
+
+		// Look for QIDs
+		const ids = text.split(/[\s\u00A0]+/); // whitespace + non-breaking space
+		let qid;
+		for (const id of ids) {
+			if ((qid = this.cleanQID(id)) && !foundIDs.has(qid)) {
+				identifiers.push({
+					extra: `qid: ${qid}`,
+				});
+				foundIDs.add(qid);
+			}
+		}
+		return identifiers;
+	}
+
 	static cleanQID(qid: string) {
 		qid = qid.toUpperCase().trim();
 		if (qid[0] !== "Q") qid = "Q" + qid;

commit f89e7c9f03ff42bfcac41943ae6d60bf6c712414

Fix bug getting items by identifier caused by 3f5ae6697c644a16e8a973c82a87d5d6ead6d852

src/cita/zoteroOverlay.tsx
@@ -280,7 +280,8 @@ class ZoteroOverlay {
 					if (identifier.extra) {
 						this.setSearch([identifier]);
 					} else {
-						original(text);
+						// needs to know `this` because it calls `this.setSearch()`
+						original.call(this, identifier);
 					}
 				},
 		);